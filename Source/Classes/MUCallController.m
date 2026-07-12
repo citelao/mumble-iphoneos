7ff80ef7c21e4ccfda6a8b4ffad45040e27b0f11
Ben Stolovitz <ben@stolovitz.com>
2026-07-12 16:55:14 -0700

    working on more events...

--- Source/Classes/MUCallController.m
+++ Source/Classes/MUCallController.m
@@ -3,13 +3,17 @@
 // license that can be found in the LICENSE file.
 
 #import "MUCallController.h"
+#import "MUConnectionController.h"
 #import <CallKit/CallKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface MUCallController () <MKServerModelDelegate, CXProviderDelegate> {
     MKConnection     *_connection;
     MKServerModel    *_model;
     CXProvider       *_cxProvider;
     CXCallController *_cxCallController;
+    
+    NSUUID           *_callUuid;
 }
 @end
 
@@ -35,17 +39,19 @@
     return self;
 }
 
+- (void) disconnectCurrentCall {
+    // End the call!
+    [[MUConnectionController sharedController] disconnectFromServer];
+    _callUuid = nil;
+}
+
 #pragma mark - MKServerModelDelegate
 
 - (void) serverModel:(MKServerModel *)model joinedServerAsUser:(MKUser *)user {
-    // TODO: start a call
-
-    // log for now:
-    NSLog(@"Joined server as user: %@", user);
-    
+    NSLog(@"Starting call - joined server as user: %@", user);
     CXHandle* handle = [[CXHandle alloc] initWithType:CXHandleTypeGeneric value:@"Test call"];
-    NSUUID *uuid = [NSUUID UUID];
-    CXStartCallAction* action = [[CXStartCallAction alloc] initWithCallUUID:uuid handle:handle];
+    _callUuid = [NSUUID UUID];
+    CXStartCallAction* action = [[CXStartCallAction alloc] initWithCallUUID:_callUuid handle:handle];
     CXTransaction* transaction = [[CXTransaction alloc] initWithAction:action];
     
     [_cxCallController requestTransaction:transaction completion:^(NSError * _Nullable error) {
@@ -57,19 +63,38 @@
 - (void) serverModelDisconnected:(MKServerModel *)model {
     // TODO: end the call
     NSLog(@"Disconnected from server, ending call");
+    
+    CXEndCallAction* action = [[CXEndCallAction alloc] initWithCallUUID:_callUuid];
+    CXTransaction* transaction = [[CXTransaction alloc] initWithAction:action];
+    
+    [_cxCallController requestTransaction:transaction completion:^(NSError * _Nullable error) {
+        // TODO: handle error
+        NSLog(@"Requested transaction to disconnect call with error: %@", error);
+    }];
 }
 
 #pragma mark - CXProviderDelegate
 
 - (void)providerDidReset:(nonnull CXProvider *)provider {
-    // TODO: disconnect from session
     NSLog(@"Call provider reset");
+    [self disconnectCurrentCall];
 }
 
 - (void)provider:(CXProvider *)provider performStartCallAction:(CXStartCallAction *)action {
     NSLog(@"Perform start call");
     
     [provider reportOutgoingCallWithUUID:action.callUUID startedConnectingAtDate:[NSDate date]];
+    
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    NSError *error = nil;
+
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord
+                    mode:AVAudioSessionModeVoiceChat
+                 options:AVAudioSessionCategoryOptionAllowBluetoothHFP
+                   error:&error];
+
+    NSLog(@"AVAudioSession config error: %@", error);
+    
     [action fulfill];
     [provider reportOutgoingCallWithUUID:action.callUUID connectedAtDate:[NSDate date]];
 }
@@ -80,6 +105,11 @@
 
 - (void)provider:(CXProvider *)provider didDeactivateAudioSession:(AVAudioSession *)audioSession {
     NSLog(@"CallKit didDeactivateAudioSession");
+}
+
+- (void)provider:(CXProvider *)provider performEndCallAction:(CXEndCallAction *)action {
+    NSLog(@"CallKit performEndCallAction");
+    [self disconnectCurrentCall];
 }
 
 @end

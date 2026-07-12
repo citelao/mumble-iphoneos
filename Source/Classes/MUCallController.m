f670f24c0fb57337055bd2665ea9043a8f965f9c
Ben Stolovitz <ben@stolovitz.com>
2026-07-11 22:58:39 -0700

    hmmm....

--- Source/Classes/MUCallController.m
+++ Source/Classes/MUCallController.m
@@ -3,10 +3,13 @@
 // license that can be found in the LICENSE file.
 
 #import "MUCallController.h"
+#import <CallKit/CallKit.h>
 
-@interface MUCallController () <MKServerModelDelegate> {
-    MKConnection    *_connection;
-    MKServerModel   *_model;
+@interface MUCallController () <MKServerModelDelegate, CXProviderDelegate> {
+    MKConnection     *_connection;
+    MKServerModel    *_model;
+    CXProvider       *_cxProvider;
+    CXCallController *_cxCallController;
 }
 @end
 
@@ -17,6 +20,17 @@
         _connection = conn;
         _model = model;
         [_model addDelegate:self];
+        
+        CXProviderConfiguration* cxConfig = [[CXProviderConfiguration alloc] initWithLocalizedName:@"Mumble"];
+        cxConfig.supportsVideo = NO;
+        cxConfig.maximumCallGroups = 1;
+        cxConfig.maximumCallsPerCallGroup = 1;
+        cxConfig.supportedHandleTypes = [NSSet setWithObject:[NSNumber numberWithInteger:CXHandleTypeGeneric]];
+        
+        _cxProvider = [[CXProvider alloc] initWithConfiguration:cxConfig];
+        [_cxProvider setDelegate:self queue:nil];
+        
+        _cxCallController = [[CXCallController alloc] init];
     }
     return self;
 }
@@ -28,11 +42,44 @@
 
     // log for now:
     NSLog(@"Joined server as user: %@", user);
+    
+    CXHandle* handle = [[CXHandle alloc] initWithType:CXHandleTypeGeneric value:@"Test call"];
+    NSUUID *uuid = [NSUUID UUID];
+    CXStartCallAction* action = [[CXStartCallAction alloc] initWithCallUUID:uuid handle:handle];
+    CXTransaction* transaction = [[CXTransaction alloc] initWithAction:action];
+    
+    [_cxCallController requestTransaction:transaction completion:^(NSError * _Nullable error) {
+        // TODO: handle error
+        NSLog(@"Requested transaction to start call with error: %@", error);
+    }];
 }
 
 - (void) serverModelDisconnected:(MKServerModel *)model {
     // TODO: end the call
     NSLog(@"Disconnected from server, ending call");
+}
+
+#pragma mark - CXProviderDelegate
+
+- (void)providerDidReset:(nonnull CXProvider *)provider {
+    // TODO: disconnect from session
+    NSLog(@"Call provider reset");
+}
+
+- (void)provider:(CXProvider *)provider performStartCallAction:(CXStartCallAction *)action {
+    NSLog(@"Perform start call");
+    
+    [provider reportOutgoingCallWithUUID:action.callUUID startedConnectingAtDate:[NSDate date]];
+    [action fulfill];
+    [provider reportOutgoingCallWithUUID:action.callUUID connectedAtDate:[NSDate date]];
+}
+
+- (void)provider:(CXProvider *)provider didActivateAudioSession:(AVAudioSession *)audioSession {
+    NSLog(@"CallKit didActivateAudioSession");
+}
+
+- (void)provider:(CXProvider *)provider didDeactivateAudioSession:(AVAudioSession *)audioSession {
+    NSLog(@"CallKit didDeactivateAudioSession");
 }
 
 @end
